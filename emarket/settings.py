2d9344a9ec8dfb6702df9f8e126590f832ec4463
fahad <fah8ad@outlook.com>
2024-07-10 20:03:42 +0300

    add

--- emarket/settings.py
+++ emarket/settings.py
@@ -38,10 +38,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-        'rest_framework',
-            'rest_framework_simplejwt',
-            "account",
-            "prodact",
+    'rest_framework',
+    'rest_framework_simplejwt',
+    "account",
+    "prodact",
+    "django_filters"
 
 
 ]
--- prodact/views.py
+++ prodact/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from .filters import ProductFilter
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -8,9 +10,8 @@ from .serializers import ProductSerializer
 
 @api_view(['GET'])
 def get_all_products(request):
-
-    products=Product.objects.all()
-    serializers = ProductSerializer(products,many=True)
+    filterSet = ProductFilter(request.GET,queryset=Product.objects.all().order_by('id'))
+    serializers = ProductSerializer(filterSet.qs,many=True)
     return Response({"products":serializers.data})
 
 @api_view(['GET'])
